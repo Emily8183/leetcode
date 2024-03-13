f6c8cf1ddaa58879b6793aa185543e8a5e104006
Emily <emily.zy.lin@gmail.com>
2024-03-13 16:28:16 -0500

    finished lc704

--- LeetCode/Search/binary_search.js
+++ LeetCode/Search/binary_search.js
@@ -1,7 +1,10 @@
+//二分查找: 左闭右闭，左闭右开。每次搜索时坚持循环不变量
+
 //binary search only works on sorted arrays
 // Original Solution
 //nothing printing out as there's only return, no console.log
 
+//以下就是左闭右闭的举例
 function binarySearch(arr, elem) {
   var start = 0;
   var end = arr.length - 1;
@@ -22,14 +25,14 @@ function binarySearch(arr, elem) {
 binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 103);
 
 // Refactored Version
-// function binarySearch(arr, elem) {
-//   var start = 0;
-//   var end = arr.length - 1;
-//   var middle = Math.floor((start + end) / 2);
-//   while (arr[middle] !== elem && start <= end) {
-//     if (elem < arr[middle]) end = middle - 1;
-//     else start = middle + 1;
-//     middle = Math.floor((start + end) / 2);
-//   }
-//   return arr[middle] === elem ? middle : -1;
-// }
+function binarySearch(arr, elem) {
+  var start = 0;
+  var end = arr.length - 1;
+  var middle = Math.floor((start + end) / 2);
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) end = middle - 1;
+    else start = middle + 1;
+    middle = Math.floor((start + end) / 2);
+  }
+  return arr[middle] === elem ? middle : -1;
+}
--- LeetCode/Search/string_search.js
+++ LeetCode/Search/string_search.js
@@ -1,12 +1,15 @@
-function naiveSearch(long, short){
-    var count = 0;
-    for(var i = 0; i < long.length; i++){
-        for(var j = 0; j < short.length; j++){
-           if(short[j] !== long[i+j]) break;
-           if(j === short.length - 1) count++;
-        }
+function naiveSearch(long, short) {
+  var count = 0;
+  for (var i = 0; i < long.length; i++) {
+    for (var j = 0; j < short.length; j++) {
+      //short string往前推进一位的同时，long string也往前推进一位
+      if (short[j] !== long[i + j]) break;
+      //在此循环中，当j能够等于short的长度-1，说明找到了
+      if (j === short.length - 1) count++;
     }
-    return count;
+  }
+  //   return count;
+  console.log(count);
 }
 
-naiveSearch("lorie loled", "lol")
+naiveSearch("lorie loled", "lol");
