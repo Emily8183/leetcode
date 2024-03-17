fd1b4711efde34e83383c793e98dd1c3dc0ef6aa
Emily <emily.zy.lin@gmail.com>
2024-03-17 00:18:43 -0500

    updated lc69

---
+++ LeetCode/InsertionSort/insertion_sort.js
@@ -0,0 +1,15 @@
+function insertionSort(arr) {
+  var currentVal;
+  for (var i = 1; i < arr.length; i++) {
+    currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+}
+
+insertionSort([2, 1, 9, 76, 4]);
+
+//如果数据来自于用户自己输入的话，这个方式很好
