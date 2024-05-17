894ca806ae5019e9e83687f4e54a1a23506c9832
Emily <emily.zy.lin@gmail.com>
2024-05-17 11:55:15 -0500

    finish lc207

--- LeetCode/Chapter2+LInkedList/lc203.js
+++ LeetCode/Chapter2+LInkedList/lc203.js
@@ -1,6 +1,6 @@
 /*
 思路：
-1）设置一个虚拟头结点，这样就不需要判断删除的这个数是否为head（如果是head, 需要把head移到下一位也怪烦的）；
+1）设置一个虚拟头结点dummyHead，这样就不需要判断删除的这个数是否为head（如果是head, 需要把head移到下一位也怪烦的）；
 2）设置一个current值作为现在的遍历对象，这个遍历对象是一个指针，指向下一个节点；
 3）遍历的前提：
 a)从虚拟头结点开始，因为需要删除的是current.next；
@@ -19,10 +19,10 @@ class ListNode {
 }
 
 var removeElements = function (head, val) {
-  let fakeHead = new ListNode(0, head);
+  let dummyHead = new ListNode(0, head);
   //值为零，指向head
 
-  let current = fakeHead;
+  let current = dummyHead;
 
   while (current.next) {
     if (current.next.val === val) {
@@ -32,5 +32,5 @@ var removeElements = function (head, val) {
     }
   }
 
-  return fakeHead.next;
+  return dummyHead.next;
 };
