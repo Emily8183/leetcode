938baefff86c6d8972d8292f18cd491eeaadc21b
Emily8183 <116020783+Emily8183@users.noreply.github.com>
2024-05-23 22:47:35 -0500

    Update lc209.js

--- LeetCode/Chapter1+Array/lc209.js
+++ LeetCode/Chapter1+Array/lc209.js
@@ -1,7 +1,7 @@
 /*
 思路：滑动窗口
 
-1）设置左边界（窗口起始点，left）及右边界（窗口终点，right)都是以0为始点。两者距离为right-left+1。设置res=Number.Max_Value，通过Math.min
+1）设置左边界（窗口起始点，left）及右边界（窗口终点，right)都是以0为始点。两者距离为right-left+1。
 2）设置两个loop, the outer loop 是right从0向右移动，the inner loop 是left向右移动。
 3）移动条件：right向右移动，直到区间内的数字之和sum大于target，停止移动；左边界向右移动，缩短两者距离right-left+1。当sum < target，继续移动右边界, 反复循环。
 4）每一次当sum大于target时，将两者距离传递给res=Number.Max_Value，不断通过Math.min更新，最终返回最小值。如果没有结果，则返回0.
